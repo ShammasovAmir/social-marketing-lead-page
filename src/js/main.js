83510f9d88b30eefe87463ea299b2cc6a0ce3ce1
Amir Shammasov <sammasovamir29@gmail.com>
2021-05-13 18:45:31 +0300

    Added proper modal styling and animation

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+const modalButtons = document.querySelectorAll('#btn')
+const modal = document.querySelector('.modal')
+const modalBg = document.querySelector('.modal-bg')
+const closeBtn = document.querySelector('.close-btn')
+
+modalButtons.forEach((modalButton) =>
+  modalButton.addEventListener('click', () => {
+    modalBg.classList.add('bg-active')
+    modal.classList.add('modal-active')
+  })
+)
+
+closeBtn.addEventListener('click', () => {
+  modalBg.classList.remove('bg-active')
+  modal.classList.remove('modal-active')
+})
